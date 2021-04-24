48442e06d184f63e17e029c7a86de4dc23293c56
Luke-Henry-Santos <lukitao.santos@gmail.com>
2021-04-24 10:54:35 -0300

    Addition of scrollview

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Alert,
   Text,
@@ -12,7 +12,7 @@ import { SvgFromUri } from 'react-native-svg'
 import { useNavigation, useRoute } from '@react-navigation/core'
 import DateTimePicker, { Event } from '@react-native-community/datetimepicker'
 import { format, isBefore } from 'date-fns'
-import { loadPlant, PlantProps, savePlant } from '../libs/storage'
+import { PlantProps, savePlant } from '../libs/storage'
 
 import waterdrop from '../assets/waterdrop.png'
 import { Button } from '../components/Button'
@@ -25,13 +25,12 @@ interface Param {
 }
 
 export function PlantSave() {
-  const [selectedDateTime, setSelectedDateTime] = useState(new Date())
-  const [showDatePicker, setShowDatePicker] = useState(Platform.OS == 'ios')
-
+  const navigation = useNavigation()
   const route = useRoute()
   const { plant } = route.params as Param
 
-  const navigation = useNavigation()
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date())
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS == 'ios')
 
   const handleChangeTime = (event: Event, dateTime: Date | undefined) => {
     if (Platform.OS === 'android')
@@ -71,119 +70,128 @@ export function PlantSave() {
   }
 
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'space-between',
-      backgroundColor: colors.shape
-    }}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{
+        flex: 1,
+        justifyContent: 'space-between',
+        backgroundColor: colors.shape
+      }}
+    >
       <View style={{
         flex: 1,
-        paddingHorizontal: 30,
-        paddingVertical: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         backgroundColor: colors.shape
       }}>
-        <SvgFromUri uri={plant.photo} height={150} width={150} />
-        <Text style={{
-          fontFamily: fonts.heading,
-          fontSize: 24,
-          color: colors.heading,
-          marginTop: 15,
-        }}>
-          {plant.name}
-        </Text>
-        <Text style={{
-          textAlign: 'center',
-          fontFamily: fonts.text,
-          color: colors.heading,
-          fontSize: 17,
-          marginTop: 10
-        }}>
-          {plant.about}
-        </Text>
-      </View>
-
-      <View style={{
-        backgroundColor: colors.white,
-        paddingHorizontal: 20,
-        paddingTop: 20,
-        paddingBottom: 50,
-      }}>
         <View style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
+          flex: 1,
+          paddingHorizontal: 30,
+          paddingVertical: 50,
           alignItems: 'center',
-          backgroundColor: colors.blue_light,
-          padding: 20,
-          borderRadius: 20,
-          position: 'relative',
-          bottom: '20%'
+          justifyContent: 'center',
+          backgroundColor: colors.shape
         }}>
-          <Image
-            source={waterdrop}
-            style={{
-              width: 56,
-              height: 56
-            }}
-          />
+          <SvgFromUri uri={plant.photo} height={150} width={150} />
           <Text style={{
-            flex: 1,
-            marginLeft: 20,
-            fontFamily: fonts.text,
-            color: colors.blue,
-            fontSize: 17,
-            textAlign: 'justify'
+            fontFamily: fonts.heading,
+            fontSize: 24,
+            color: colors.heading,
+            marginTop: 15,
           }}>
-            {plant.water_tips}
+            {plant.name}
+          </Text>
+          <Text style={{
+            textAlign: 'center',
+            fontFamily: fonts.text,
+            color: colors.heading,
+            fontSize: 17,
+            marginTop: 10
+          }}>
+            {plant.about}
           </Text>
         </View>
 
-        <Text style={{
-          textAlign: 'center',
-          fontFamily: fonts.complement,
-          color: colors.heading,
-          fontSize: 12,
-          marginBottom: 5
+        <View style={{
+          backgroundColor: colors.white,
+          paddingHorizontal: 20,
+          paddingTop: 20,
+          paddingBottom: 50,
         }}>
-          Escolha o melhor horário para ser lembrado:
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: colors.blue_light,
+            padding: 20,
+            borderRadius: 20,
+            position: 'relative',
+            bottom: '20%'
+          }}>
+            <Image
+              source={waterdrop}
+              style={{
+                width: 56,
+                height: 56
+              }}
+            />
+            <Text style={{
+              flex: 1,
+              marginLeft: 20,
+              fontFamily: fonts.text,
+              color: colors.blue,
+              fontSize: 17,
+              textAlign: 'justify'
+            }}>
+              {plant.water_tips}
+            </Text>
+          </View>
+
+          <Text style={{
+            textAlign: 'center',
+            fontFamily: fonts.complement,
+            color: colors.heading,
+            fontSize: 12,
+            marginBottom: 5
+          }}>
+            Escolha o melhor horário para ser lembrado:
           </Text>
 
-        {showDatePicker && (
-          <DateTimePicker
-            value={selectedDateTime}
-            mode="time"
-            display="spinner"
-            onChange={handleChangeTime}
+          {showDatePicker && (
+            <DateTimePicker
+              value={selectedDateTime}
+              mode="time"
+              display="spinner"
+              onChange={handleChangeTime}
+            />
+          )}
+
+          {
+            Platform.OS === 'android' && (
+              <TouchableOpacity
+                style={{
+                  width: '100%',
+                  alignItems: 'center',
+                  paddingVertical: 40,
+                }}
+                onPress={handleOpenDateTimeAndroid}
+              >
+                <Text style={{
+                  color: colors.heading,
+                  fontSize: 24,
+                  fontFamily: fonts.text
+                }}>
+                  {`Mudar ${format(selectedDateTime, 'HH:mm')}`}
+                </Text>
+              </TouchableOpacity>
+            )
+          }
+
+
+          <Button title="Cadastrar planta"
+            onPress={handleSave}
           />
-        )}
-
-        {
-          Platform.OS === 'android' && (
-            <TouchableOpacity
-              style={{
-                width: '100%',
-                alignItems: 'center',
-                paddingVertical: 40,
-              }}
-              onPress={handleOpenDateTimeAndroid}
-            >
-              <Text style={{
-                color: colors.heading,
-                fontSize: 24,
-                fontFamily: fonts.text
-              }}>
-                {`Mudar ${format(selectedDateTime, 'HH:mm')}`}
-              </Text>
-            </TouchableOpacity>
-          )
-        }
-
-
-        <Button title="Cadastrar planta"
-          onPress={handleSave}
-        />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
