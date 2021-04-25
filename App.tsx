a59938398811c152f3e9d217cf0ce731b03d91a1
Luke-Henry-Santos <lukitao.santos@gmail.com>
2021-04-25 14:30:56 -0300

    Assigning the context of Plants and My Plants

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import AppLoading from 'expo-app-loading'
-
 import Routes from './src/routes';
+
+import { PlantContextProvider } from './src/context/PlantContext';
+import { MyPlantsContextProvider } from './src/context/MyPlantsContext';
 
 import {
   useFonts,
@@ -10,16 +12,17 @@ import {
   Jost_600SemiBold
 } from '@expo-google-fonts/jost'
 
-
 export default function App() {
   const [fontsLoaded] = useFonts({ Jost_400Regular, Jost_600SemiBold })
 
   if (!fontsLoaded) return <AppLoading />
 
   return (
-    <>
-      <StatusBar style='auto' />
-      <Routes />
-    </>
+    <PlantContextProvider>
+      <MyPlantsContextProvider>
+        <StatusBar style='auto' />
+        <Routes />
+      </MyPlantsContextProvider>
+    </PlantContextProvider>
   );
 }
